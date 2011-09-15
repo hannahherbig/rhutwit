9539e8872e3702ac622219677825ec4c09554a97
Andrew Herbig <goforit7arh@gmail.com>
2011-09-14 20:22:16 -0400

    Handle exceptions in io_loop

--- stream.rb
+++ stream.rb
@@ -44,36 +44,48 @@ class Stream
         end
     end
 
-    def io_loop
-        loop do
-            uri = URI.parse("https://stream.twitter.com/1/statuses/filter.json")
-            post_body = "follow=#{@config.users.join(',')}"
+    uri = URI.parse()
+    post_body = "follow=#{@config.users.join(',')}"
 
-            http = Net::HTTP.new uri.host, uri.port
-            http.use_ssl = true
+    http = Net::HTTP.new uri.host, uri.port
+    http.use_ssl = true
 
-            http.start do |http|
-                req = Net::HTTP::Post.new(uri.path)
-                req.basic_auth @config.username, @config.password
-                http.request(req, post_body) do |res|
-                    res.read_body do |seg|
-                        # This passes every "line" to our block, including the "\n".
-                        seg.scan(/(.+\n?)/) do |line|
-                            line = line[0]
+    http.start do |http|
+        req = Net::HTTP::Post.new(uri.path)
+        req.basic_auth @config.username, @config.password
+        http.request(req, post_body) do |res|
+            res.read_body do |seg|
+                # This passes every "line" to our block, including the "\n".
+                seg.scan(/(.+\n?)/) do |line|
+                    line = line[0]
 
-                            # If the last line had no \n, add this one onto it.
-                            if @recvq[-1] and @recvq[-1][-1].chr != "\n"
-                                @recvq[-1] += line
-                            else
-                                @recvq << line
-                            end
-                        end
-
-                        if @recvq[-1] and @recvq[-1][-1].chr == "\n"
-                            parse
-                        end
+                    # If the last line had no \n, add this one onto it.
+                    if @recvq[-1] and @recvq[-1][-1].chr != "\n"
+                        @recvq[-1] += line
+                    else
+                        @recvq << line
                     end
                 end
+
+                if @recvq[-1] and @recvq[-1][-1].chr == "\n"
+                    parse
+                end
+            end
+        end
+    end
+
+    def io_loop
+        loop do
+            begin
+                start
+            rescue Exception => e
+                log.error("#{e.class.name}: #{e.message}")
+
+                e.backtrace.each do |line|
+                    log.error("\t#{line}")
+                end
+            else
+                log.error "died for an unknown reason"
             end
         end
     end
